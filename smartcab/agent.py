74598c1fb5e1f2fd5998fd82c023b6a379ff25a6
Sam Leo <sandyleo26@gmail.com>
2017-07-14 00:25:18 +1000

    increase trials to reach highest safety

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -191,7 +191,7 @@ def run():
     #   learning   - set to True to force the driving agent to use Q-learning
     #    * epsilon - continuous value for the exploration factor, default is 1
     #    * alpha   - continuous value for the learning rate, default is 0.5
-    agent = env.create_agent(LearningAgent, learning=learning, alpha=0.95)
+    agent = env.create_agent(LearningAgent, learning=learning, alpha=0.99)
     
     ##############
     # Follow the driving agent
